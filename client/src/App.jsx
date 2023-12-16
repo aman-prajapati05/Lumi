28ed91f6a0473294ddfe451897ca3f927e71779f
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2023-12-16 21:42:09 +0530

    commit-2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 import ImageInput from "./components/Image-input";
 import "./index.css";
-import Navbar from './components/Navbar.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
+    <div className="App">
+      <Hero />
+      <Navbar />
+      <ImageInput />
+    </div>
+  );
+}
+
+export default App;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/Hero.jsx';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
     <>
-      <Hero/>
+      <Hero />
+      <Navbar />
     </>
-  )
+  );
 }
 
 export default App
+
